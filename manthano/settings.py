1611d186d6fa7427ed3c6081d505e7841f76f19e
reszkojr <s.reszkojr@gmail.com>
2023-04-08 19:36:10 -0300

    main app basic configurations

--- manthano/settings.py
+++ manthano/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'crispy_bootstrap5'
+    'crispy_bootstrap5',
     'main.apps.MainConfig',
 ]
 
