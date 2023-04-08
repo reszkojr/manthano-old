5f70b808b47e83620a53562439a028765cbfdbb0
reszkojr <s.reszkojr@gmail.com>
2023-04-08 19:21:50 -0300

    crispy and main apps

--- manthano/settings.py
+++ manthano/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5'
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
