74750066640aa411c53937dff3c1b2ad807e8c98
reszkojr <s.reszkojr@gmail.com>
2023-04-08 21:36:12 -0300

    some basic modifications

--- main/forms.py
+++ main/forms.py
@@ -3,9 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
