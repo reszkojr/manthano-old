111388b5df8bd03ae88861839d3ecfc8c70150ee
reszkojr <s.reszkojr@gmail.com>
2023-04-08 21:38:05 -0300

    login required to access home

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 from .forms import RegistrationForm
 
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'main/home.html')
 
