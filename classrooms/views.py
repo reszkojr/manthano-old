dd0299bb8b113227e9af2ec6a554199a9d4aacdd
reszkojr <s.reszkojr@gmail.com>
2023-04-10 20:27:03 -0300

    join_classroom logic

--- classrooms/views.py
+++ classrooms/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ClassroomCreationForm
+
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import redirect, render
+
+@login_required(login_url='/login')
+def join_classroom(request):
+    # The user does not have a classroom yet
+    if not request.user.classroom:
+        return redirect(request, 'classroom/join_or_create.html')
+    if request.method == 'POST':
+        form = ClassroomCreationForm(request.POST)
+        if form.is_valid():
+            classroom = ClassroomCreationForm.save(commit=False)
+            classroom.save()
+            return redirect('/home')
+    else:
+        form = ClassroomCreationForm()
+
+    return render(request, 'classroom/create_classroom.html')
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+import classrooms
+
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('sign-up', views.sign_up, name='sign_up'),
+    path('classroom', classrooms.views.join_classroom, name='sign_up'),
 ]
