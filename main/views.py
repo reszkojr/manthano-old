254f995a97c8df263de71ac435958533f223a563
reszkojr <s.reszkojr@gmail.com>
2023-04-29 14:32:03 -0300

    login_required fixed

--- main/views.py
+++ main/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-@login_required()
+@login_required(login_url='/user/login')
 def home(request):
     return render(request, 'main/home.html')
