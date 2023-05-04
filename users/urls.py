e7013172b567725878aef8ea2273af5e025e8fbe
reszkojr <s.reszkojr@gmail.com>
2023-05-04 07:23:39 -0300

    media folder

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
@@ -12,4 +14,5 @@ urlpatterns = [
     path('sign-up/', views.sign_up, name='sign_up'),
     path('logout/', views.log_out, name='log_out'),
     path('profile/', views.profile, name='profile'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
