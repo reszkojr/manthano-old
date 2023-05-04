9003f33a58a39d86d560bf213cd90edd1c77da18
reszkojr <s.reszkojr@gmail.com>
2023-05-04 07:23:39 -0300

    media folder

--- manthano/settings.py
+++ manthano/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for manthano project.
 
@@ -108,7 +110,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -144,6 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
