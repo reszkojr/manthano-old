f7ad513b37ac5a70d61d81d6b10ddcddd1208c84
Fabio S. Reszko Jr <67809084+reszkojr@users.noreply.github.com>
2023-05-08 07:52:57 +0000

    django signals implementation on user - profile relation

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    # Registering signals on Django initialization
+    def ready(self):
+        import users.signals
